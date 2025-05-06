a96ebc515bd2fecdf84f6725bfa5e3b50c9bcbaf
eric2003tu <mukarugambwaelevanie@gmail.com>
2025-05-06 14:20:51 +0200

    about products is almost to done

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Components/Home';
 import Products from './Components/Products';
 import Cart from './Components/Cart';
